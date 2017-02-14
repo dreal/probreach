37cb202b43fd56fa0124d6bea5e28886f652ef33
shmarovfedor <f.shmarov@ncl.ac.uk>
2017-02-14 14:06:03 +0000

    updated inslulin model

--- test/pdrh2smt/node_test.cpp
+++ test/pdrh2smt/node_test.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 TEST(string_to_infix, normal)
 {
-    EXPECT_STREQ(string("a").c_str(), node("a").to_string_infix().c_str());
-    EXPECT_STREQ(string("(a+b)").c_str(), node("+",vector<node>{node("a"), node("b")}).to_string_infix().c_str());
+    EXPECT_STREQ(string("a").c_str(), node("a").to_infix().c_str());
+    EXPECT_STREQ(string("(a+b)").c_str(), node("+",vector<node>{node("a"), node("b")}).to_infix().c_str());
 }
 
 TEST(string_to_prefix, normal)
 {
-    EXPECT_STREQ(string(" a").c_str(), node("a").to_string_prefix().c_str());
-    EXPECT_STREQ(string("(+ a b)").c_str(), node("+",vector<node>{node("a"), node("b")}).to_string_prefix().c_str());
+    EXPECT_STREQ(string(" a").c_str(), node("a").to_prefix().c_str());
+    EXPECT_STREQ(string("(+ a b)").c_str(), node("+",vector<node>{node("a"), node("b")}).to_prefix().c_str());
 }
 
 TEST(append, normal)
@@ -25,5 +25,5 @@ TEST(append, normal)
     node minus("-", vector<node>{node("d"), node("e"), node("f")});
     node mult("*");
     mult.append(vector<node>{plus, minus});
-    EXPECT_STREQ(string("((a+b+c)*(d-e-f))").c_str(), mult.to_string_infix().c_str());
+    EXPECT_STREQ(string("((a+b+c)*(d-e-f))").c_str(), mult.to_infix().c_str());
 }

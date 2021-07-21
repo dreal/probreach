f843d278997e99943febfa11fe7cfbc0512e0e30
shmarovfedor <shmarovfedor@mail.ru>
2021-07-21 13:15:57 +0100

    updated formatting

--- src/util/generators/smt2_generator.h
+++ src/util/generators/smt2_generator.h
@@ -5,39 +5,43 @@
 #ifndef PROBREACH_SMT2_GENERATOR_H
 #define PROBREACH_SMT2_GENERATOR_H
 
-#include <iostream>
 #include <capd/capdlib.h>
-#include "model.h"
+
+#include <iostream>
+
 #include "box.h"
+#include "model.h"
 
-namespace smt2_generator
-{
+namespace smt2_generator {
 
-    // generates the formula for the invariants check
-    // first argument contains the odes to compute and the invariants to check
-    // second arguments is the time horizon for ode integration
-    // third argument is the interval initial condition for the ode system
-    // fourth argument is the vector of samples
-    std::string generate_flow_invt_check(pdrh::mode*, capd::interval, box, vector<box>);
+// generates the formula for the invariants check
+// first argument contains the odes to compute and the invariants to check
+// second arguments is the time horizon for ode integration
+// third argument is the interval initial condition for the ode system
+// fourth argument is the vector of samples
+std::string generate_flow_invt_check(pdrh::mode*, capd::interval, box,
+                                     vector<box>);
 
-    // generates the complement formula for the invariants check
-    // first argument contains the odes to compute and the invariants to check
-    // second arguments is the time horizon for ode integration
-    // third argument is the interval initial condition for the ode system
-    // fourth argument is the vector of samples
-    std::string generate_flow_invt_check_c(pdrh::mode*, capd::interval, box, vector<box>);
+// generates the complement formula for the invariants check
+// first argument contains the odes to compute and the invariants to check
+// second arguments is the time horizon for ode integration
+// third argument is the interval initial condition for the ode system
+// fourth argument is the vector of samples
+std::string generate_flow_invt_check_c(pdrh::mode*, capd::interval, box,
+                                       vector<box>);
 
-    // generates the formula to check the jumps
-    // first argument contains the odes to compute and the jumps to check
-    // second argument is the interval initial condition for the ode system
-    // third argument is the vector of samples
-    std::string generate_jump_check(pdrh::mode*, vector<pdrh::mode::jump>, box, vector<box>);
+// generates the formula to check the jumps
+// first argument contains the odes to compute and the jumps to check
+// second argument is the interval initial condition for the ode system
+// third argument is the vector of samples
+std::string generate_jump_check(pdrh::mode*, vector<pdrh::mode::jump>, box,
+                                vector<box>);
 
-    // generates reachability formulas
-    std::string reach_to_smt2(std::vector<pdrh::mode*>, std::vector<box>);
-    std::string reach_c_to_smt2(std::vector<pdrh::mode*>, std::vector<box>);
-    std::string reach_c_to_smt2(int, std::vector<pdrh::mode*>, vector<box>);
+// generates reachability formulas
+std::string reach_to_smt2(std::vector<pdrh::mode*>, std::vector<box>);
+std::string reach_c_to_smt2(std::vector<pdrh::mode*>, std::vector<box>);
+std::string reach_c_to_smt2(int, std::vector<pdrh::mode*>, vector<box>);
 
-}
+}  // namespace smt2_generator
 
-#endif //PROBREACH_SMT2_GENERATOR_H
+#endif  // PROBREACH_SMT2_GENERATOR_H

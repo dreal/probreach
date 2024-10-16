11b3ca994f1996b1a8b344a7da70a66cb50fc4ea
Fedor Shmarov <shmarovfedor@mail.ru>
2024-10-16 14:58:44 +0100

    updated building scripts, CAPD cmake and GTest c++14 dependency

--- cmake/Modules/FindCAPD.cmake
+++ cmake/Modules/FindCAPD.cmake
@@ -1,44 +1,26 @@
-# - Finding CAPD library
-#  CAPD_FOUND - CAPD is found
-#  CAPD_INCLUDE_DIR - CAPD include directories
-#  CAPD_LIBRARIES - CAPD library
-
-set(CAPD_INCLUDE_DIR_LIST ${CAPD_INCLUDE_DIR_LIST} /usr/capd/include)
-set(CAPD_INCLUDE_DIR_LIST ${CAPD_INCLUDE_DIR_LIST} /usr/local/capd/include)
-
-#set(CAPD_LIBRARIES_LIST ${CAPD_LIBRARIES_LIST} /usr/capd/lib/libcapd-gui.a)
-#set(CAPD_LIBRARIES_LIST ${CAPD_LIBRARIES_LIST} /usr/local/capd/lib/libcapd-gui.a)
-
-set(CAPD_LIBRARIES_LIST ${CAPD_LIBRARIES_LIST} /usr/capd/lib/libcapd.a)
-set(CAPD_LIBRARIES_LIST ${CAPD_LIBRARIES_LIST} /usr/local/capd/lib/libcapd.a)
+# - Finding CAPD library. Variable that will be set if CAPD is found:
+#
+#       CAPD_FOUND - CAPD is found
+#       CAPD_INCLUDE_DIR - CAPD include directories
+#       CAPD_LIBRARIES - CAPD library
 
 if(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
-    set(CAPD_FOUND TRUE)
+  set(CAPD_FOUND ON)
 else(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
 
-    # finding include directories
-    foreach(ITEM ${CAPD_INCLUDE_DIR_LIST})
-        if(EXISTS ${ITEM} AND NOT CAPD_INCLUDE_DIR)
-            set(CAPD_INCLUDE_DIR ${ITEM})
-        endif(EXISTS ${ITEM} AND NOT CAPD_INCLUDE_DIR)
-    endforeach(ITEM)
+  # searching for includes
+  if(EXISTS ${CAPD_INSTALL_PATH}/include/capd)
+    set(CAPD_INCLUDE_DIR "${CAPD_INSTALL_PATH}/include")
+  endif(EXISTS ${CAPD_INSTALL_PATH}/include/capd)
 
-    # finding libraries
-    foreach(ITEM ${CAPD_LIBRARIES_LIST})
-        if(EXISTS ${ITEM} AND NOT CAPD_LIBRARIES)
-            set(CAPD_LIBRARIES ${ITEM})
-        endif(EXISTS ${ITEM} AND NOT CAPD_LIBRARIES)
-    endforeach(ITEM)
+  # searching for libraries
+  if(EXISTS ${CAPD_INSTALL_PATH}/lib/libcapd.a)
+    set(CAPD_LIBRARIES "${CAPD_INSTALL_PATH}/lib/libcapd.a")
+  endif(EXISTS ${CAPD_INSTALL_PATH}/lib/libcapd.a)
 
-    # checking if include directories and libraries has been found
-    if(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
-        set(CAPD_FOUND TRUE)
-    endif(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
+  # checking if include directories and libraries has been found
+  if(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
+    set(CAPD_FOUND ON)
+  endif(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
+
 endif(CAPD_INCLUDE_DIR AND CAPD_LIBRARIES)
-
-# reporting whether CAPD has been found
-if(CAPD_FOUND)
-    message(STATUS "Found CAPD (includes: ${CAPD_INCLUDE_DIR}, libs: ${CAPD_LIBRARIES})")
-else(CAPD_FOUND)
-    message(STATUS "CAPD not found")
-endif(CAPD_FOUND)

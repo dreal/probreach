02922de593ed1e67249e993687fd8375ba494e0d
Fedor Shmarov <shmarovfedor@mail.ru>
2024-10-19 20:15:08 +0100

    added more functional tests for good and bad PHAs

--- test/functional/formal/good_test_1.cpp
+++ test/functional/formal/good_test_1.cpp
@@ -10,16 +10,16 @@
 #include "box.h"
 #include "pdrh2box.h"
 #include "decision_procedure.h"
-#include "logging/easylogging++.h"
 #include "solver/dreal_wrapper.h"
+#include "test_env.h"
 
 #ifdef _OPENMP
-  #include <omp.h>
+#include <omp.h>
 #endif
 
 extern "C"
 {
-  #include "pdrhparser.h"
+#include "pdrhparser.h"
 }
 
 extern "C" int yyparse();
@@ -27,13 +27,13 @@ extern "C" FILE *yyin;
 
 using namespace std;
 
-TEST(formal_good_test, testing_good_1_pdrh)
+static void parse_pdrh_model(string filepath)
 {
   // opening the model file
-  FILE *pdrhfile = fopen("/home/fedor/probreach/test/functional/models/good/good_1.pdrh", "r");
+  FILE *pdrhfile = fopen(filepath.c_str(), "r");
   if (!pdrhfile)
   {
-    cerr << "Couldn't open the file: " << endl;
+    cerr << "Couldn't open the file: " << filepath << endl;
     exit(EXIT_FAILURE);
   }
   // set lex to read from it instead of defaulting to STDIN:
@@ -43,14 +43,19 @@ TEST(formal_good_test, testing_good_1_pdrh)
   {
     yyparse();
   } while (!feof(yyin));
+}
+
+TEST(formal_good_test_1, testing_good_1_pdrh)
+{
+  parse_pdrh_model(
+    string(PROBREACH_TEST_MODELS_DIR) + string("/good/good_1.pdrh"));
   // setting precision for computing the probability interval
   global_config.precision_prob = 1e-3;
-  // computing the probability now  
+  // computing the probability now
   capd::interval probability = formal::evaluate_pha(0, 0);
   // true probability in this example is 0.1; so the probability interval:
   // 1) must contain 0.1
   // 2) its width must be <= "global_config.precision_prob"
-  EXPECT_TRUE(probability.contains(capd::interval("0.1","0.1")));
+  EXPECT_TRUE(probability.contains(capd::interval("0.1", "0.1")));
   EXPECT_LE(capd::intervals::width(probability), global_config.precision_prob);
 }
-
---
+++ test/functional/test_env.h
@@ -0,0 +1,3 @@
+// A set of environment variables for testing
+#define PROBREACH_TEST_DIR "/home/fedor/probreach/test"
+#define PROBREACH_TEST_MODELS_DIR "/home/fedor/probreach/test/functional/models"
